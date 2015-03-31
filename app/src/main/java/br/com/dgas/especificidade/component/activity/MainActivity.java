2aebec9a54d73e4876679c8664c49df3cedeee46
diegogeovani <diegogeovani@aol.com>
2015-03-31 14:02:45 -0300

    refatoracao: pacote component.activities renomeado p/ component.activity

--- app/src/main/java/br/com/dgas/especificidade/component/activity/MainActivity.java
+++ app/src/main/java/br/com/dgas/especificidade/component/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package br.com.dgas.especificidade.component.activities;
+package br.com.dgas.especificidade.component.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
