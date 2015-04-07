536446d898c471669e6d3cc526bcfb1200bd4a97
diegogeovani <diegogeovani@aol.com>
2015-04-07 17:11:22 -0300

    estrutura inicial do request p/ verificacao da base de dados

--- app/src/main/java/br/com/dgas/especificidade/component/activity/LoadingActivity.java
+++ app/src/main/java/br/com/dgas/especificidade/component/activity/LoadingActivity.java
@@ -7,39 +7,38 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.io.File;
-
 import br.com.dgas.especificidade.R;
 import br.com.dgas.especificidade.os.task.SinglePlayerDatabaseCheckTask;
 
 public class LoadingActivity extends ActionBarActivity
-        implements SinglePlayerDatabaseCheckTask.OnTaskCompleteListener {
+        implements SinglePlayerDatabaseCheckTask.OnTaskResultListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        this.startSinglePlayerDataBaseTask(3000);
+        this.startSinglePlayerTask(3000);
     }
 
     @SuppressWarnings("SameParameterValue")
-    private void startSinglePlayerDataBaseTask(int delay) {
+    private void startSinglePlayerTask(int delay) {
         final SinglePlayerDatabaseCheckTask singlePlayerTask =
                 new SinglePlayerDatabaseCheckTask(this);
+        final String singlePlayerUrl = getString(R.string.url_singleplayer_version_checker);
 
         if (delay > 0) {
             Handler handler = new Handler();
             Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                    singlePlayerTask.execute();
+                    singlePlayerTask.execute(singlePlayerUrl);
                 }
             };
             handler.postDelayed(runnable, delay);
 
         } else {
-            singlePlayerTask.execute();
+            singlePlayerTask.execute(singlePlayerUrl);
         }
     }
 
@@ -65,7 +64,12 @@ public class LoadingActivity extends ActionBarActivity
     }
 
     @Override
-    public void onTaskComplete(File file) {
+    public void onTaskCancelled() {
+        finish();
+    }
+
+    @Override
+    public void onTaskComplete() {
         if (!isFinishing()) {
             finish();
             this.displayMainScreen();
--- app/src/main/java/br/com/dgas/especificidade/os/task/SinglePlayerDatabaseCheckTask.java
+++ app/src/main/java/br/com/dgas/especificidade/os/task/SinglePlayerDatabaseCheckTask.java
@@ -2,29 +2,65 @@ package br.com.dgas.especificidade.os.task;
 
 import android.os.AsyncTask;
 
-import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class SinglePlayerDatabaseCheckTask extends AsyncTask<String, Void, File> {
+public class SinglePlayerDatabaseCheckTask extends AsyncTask<String, Void, Boolean> {
 
-    private final OnTaskCompleteListener listener;
+    private static final int CONNECTION_TIMEOUT = 5000;
+    private static final String NULL_RESULT_ERROR_MESSAGE = "Null result. The task could not " +
+            "be cancelled because something went wrong.";
+    private final OnTaskResultListener listener;
 
-    public SinglePlayerDatabaseCheckTask(OnTaskCompleteListener listener) {
+    public SinglePlayerDatabaseCheckTask(OnTaskResultListener listener) {
         this.listener = listener;
     }
 
     @Override
-    protected File doInBackground(String... params) {
-        return null;
+    protected Boolean doInBackground(String... params) {
+        try {
+            URL url = new URL(params[0]);
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setConnectTimeout(CONNECTION_TIMEOUT);
+            httpConnection.connect();
+
+            return true;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            cancel(true);
+            return null;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            cancel(true);
+            return null;
+        }
     }
 
     @Override
-    protected void onPostExecute(File file) {
-        super.onPostExecute(file);
-        listener.onTaskComplete(file);
+    protected void onCancelled() {
+        super.onCancelled();
+        listener.onTaskCancelled();
     }
 
-    public interface OnTaskCompleteListener {
-        void onTaskComplete(File file);
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (result != null) {
+            listener.onTaskComplete();
+        } else {
+            throw new IllegalArgumentException(NULL_RESULT_ERROR_MESSAGE);
+        }
+    }
+
+    public interface OnTaskResultListener {
+        void onTaskCancelled();
+
+        void onTaskComplete();
     }
 
 }
